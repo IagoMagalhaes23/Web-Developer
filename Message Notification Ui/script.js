649ee3c3a6f7f33d7598cb0b445cc016af81491b
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2024-02-09 22:34:04 -0300

    update 'script.js'

--- Message Notification Ui/script.js
+++ Message Notification Ui/script.js
@@ -2,5 +2,9 @@ const bell = document.querySelector('.icon-tabler-bell');
 const notificationContainer = document.querySelector('.notification');
 
 bell.addEventListener('click', () => {
-    notificationContainer.style.transform = 'scale(0)';
+    if(notificationContainer.style.transform === 'scale(0)'){
+        notificationContainer.style.transform = 'scale(1)';
+    }else{
+        notificationContainer.style.transform = 'scale(0)';
+    }
 })
